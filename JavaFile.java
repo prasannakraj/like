591f439a4f05861099548d1185caa780b8b2bcec
prasannakraj <89721892+prasannakraj@users.noreply.github.com>
2021-08-29 12:25:31 +0530

    Created Object

--- JavaFile.java
+++ JavaFile.java
@@ -1,5 +1,10 @@
 public class FirstGit{
-    public static void main(){
+        void run(){
         System.out.println("Welcome to GitHub");
-}
+        System.out.println("Develop and Load it");
+        }
+    public static void main(){
+        FirstGit git = new FirstGit();
+        git.run();
+    }
 }
